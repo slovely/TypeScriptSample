857dfc31e551b2020627a9226f615c4de5f18ae2
Simon Lovely <s.lovely@gmail.com>
2014-11-06 18:12:44 +0000

    Updated WebAPI call to include the response type

--- src/TypeScriptSample.Web/App/app.ts
+++ src/TypeScriptSample.Web/App/app.ts
@@ -8,8 +8,7 @@ function getPeople() {
     $.ajax({
         url: "api/person",
         method: 'get',
-        // response could be anything here
-    }).done((response) => {
+    }).done((response: Array<TypeScriptSample.Models.Person>) => {
         var details = '<ul>';
         for (var i = 0; i < response.length; i++) {
             details += "<li>" + response[i].Name + "</li>"; //If 'Name' gets changed on the server, this code will fail
